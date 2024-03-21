f5073a9a4207ade8412922990bd2ef52b81d32f0
luca <lucacandia100@gmail.com>
2024-03-21 01:29:00 -0300

    comentario y carpeta

---
+++ lib/logica/calculo.dart
@@ -0,0 +1,11 @@
+calcular(int dineroUnPago, int cantCuotas){
+  
+  //funcion para calcular el financimianto
+  //dineroUnPago: Cantidad de dinero que la persona esta dispuesta a pagar en un solo pago.
+  //cantCuotas: Cantidad de cuotas deseadas por las persona. 
+
+  double costoCuota;
+
+
+
+}
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class mainScreen extends StatelessWidget {
+  const mainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("FINANCIAMIENTO",
+        style: TextStyle(fontSize: 16,)),
+
+
+      ],
+    );
+  }
+}
