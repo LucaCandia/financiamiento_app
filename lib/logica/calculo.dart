25712a3d096f9e2de67376cafd9692f004ce2127
Luca Tocce <luca.toccce1@gmail.com>
2024-03-21 01:27:53 -0300

    calculo realizado

--- lib/logica/calculo.dart
+++ lib/logica/calculo.dart
@@ -1,5 +1,11 @@
-calcular(int dineroUnPago, int cantCuotas){
+calcular( costoMoto, montoPagoUnico,  cantCuotas){
+  //El usuario paga un monto en un pago, lo que se financia en cuotas es el resto que queda por pagar
+  //segun la cantidad de cuotas se aplica cierto interes, en este caso esta hardcodeado
+  //El metodo devuelve el valor de cada cuota.
   double costoCuota;
-  
 
+  costoCuota = (costoMoto - montoPagoUnico)/cantCuotas;
+  costoCuota = costoCuota*25;
+
+  return costoCuota;
 }
