8144df8517176aacc09142b4ec16f36559d70dde
luca <lucacandia100@gmail.com>
2024-03-21 01:09:20 -0300

    titulo

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FINACIAMIENTO',
       theme: ThemeData(
 
         // This works for code too, not just values: Most code changes can be
